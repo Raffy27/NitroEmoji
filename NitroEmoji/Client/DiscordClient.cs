c0fa559dc3d5d894055c00a0cf2bad4c62f94dbd
Raffy Efrényi <raffy27@pm.me>
2020-05-29 18:01:59 +0300

    Login system

---
+++ NitroEmoji/Client/DiscordClient.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NitroEmoji.Client
+{
+    class DiscordClient
+    {
+        public string Token;
+
+        public async Task<bool> Login(string email, string pass) {
+            WebClient w = new WebClient();
+
+            string payload = $"{{\"email\":\"{email}\",\"password\":\"{pass}\",\"undelete\":false,\"captcha_key\":null,\"login_source\":null,\"gift_code_sku_id\":null}}";
+            w.Headers.Set("Content-Type", "application/json");
+            try {
+                var res = await w.UploadStringTaskAsync("https://discord.com/api/v6/auth/login", payload);
+                dynamic data = JObject.Parse(res);
+                Token = data.token;
+                return true;
+            } catch (WebException e) {
+                var response = e.Response as HttpWebResponse;
+                if (response != null) {
+                    var res = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    Debug.WriteLine("Login failed: {0} => {1}", (int)response.StatusCode, res);
+                }                
+                return false;
+            }
+        }
+    }
+}
---
+++ NitroEmoji/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using NitroEmoji.Client;
+
+namespace NitroEmoji
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        private DiscordClient C = new DiscordClient();
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e) {
+            StatusLabel.Content = "Logging in...";
+            Task<bool> Login = C.Login(EmailBox.Text, PasswordBox.Password);
+            LoginButton.IsEnabled = false;
+            bool success = await Login;
+            if (!success) {
+                StatusLabel.Content = "Login failed";
+                LoginButton.IsEnabled = true;
+            } else {
+                StatusLabel.Content = "Login successful";
+                LoginContainer.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ClearDefault(object sender, RoutedEventArgs e) {
+            if(sender is PasswordBox) {
+                var t = sender as PasswordBox;
+                if (t.Password == "Password") {
+                    t.Password = "";
+                }
+            } else {
+                var t = sender as TextBox;
+                if (t.Text == "Email") {
+                    t.Text = "";
+                }
+            }
+        }
+    }
+}
